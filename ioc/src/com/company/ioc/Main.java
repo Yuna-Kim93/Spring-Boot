51a994b9fcb5868f91756adab0519cf8bf7cc13d
Yuna-Kim93 <rladbsdk93@gmail.com>
2021-07-02 23:34:34 +0900

    Ioc, DI

--- ioc/src/com/company/ioc/Main.java
+++ ioc/src/com/company/ioc/Main.java
@@ -5,7 +5,13 @@ public class Main {
     public static void main(String[] args) {
 
         String url = "www.naver.com/books/it?page=10&size=20&name=spring-boot";
-        
-        System.out.println("main");
+
+        // Base 64 encoding
+
+        // url encoding
+        Encoder encoder = new Encoder(new UrlEncoder());
+        String result = encoder.encode(url);
+
+        System.out.println(result);
     }
 }
